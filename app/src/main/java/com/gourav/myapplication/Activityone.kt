edbb493b78f424a374f4994f65b7450f36bdcd6c
gaurav <jaingaurav112@gmail.com>
2019-09-15 23:11:54 +0530

    Arc floating Button

--- app/src/main/java/com/gourav/myapplication/Activityone.kt
+++ app/src/main/java/com/gourav/myapplication/Activityone.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity1.*
+import com.sa90.materialarcmenu.StateChangeListener
+
+
 
 class Activityone:AppCompatActivity(){
 
@@ -14,21 +17,30 @@ class Activityone:AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity1)
 
-        card.setOnClickListener {
-            var intent=Intent(this,Activity2::class.java)
 
-            val pairs = arrayOfNulls<android.util.Pair<View, String>>(3)
+        arcmenu_android_example_layout.setStateChangeListener(object : StateChangeListener {
+            override fun onMenuOpened() {
+                //TODO something when menu is opened
+            }
 
-
-//            pairs[0]= Pair(imageView,"imageview")
-//            pairs[1]=Pair(textView2,"textview")
-
-            val options = ActivityOptions.makeSceneTransitionAnimation(this, *pairs)
-
-
-
-
-            startActivity(intent)
+            override fun onMenuClosed() {
+                //TODO something when menu is closed
+            }
+        })
+//        card.setOnClickListener {
+//            var intent=Intent(this,Activity2::class.java)
+//
+//            val pairs = arrayOfNulls<android.util.Pair<View, String>>(3)
+//
+//
+////            pairs[0]= Pair(imageView,"imageview")
+////            pairs[1]=Pair(textView2,"textview")
+//
+//            val options = ActivityOptions.makeSceneTransitionAnimation(this, *pairs)
+//
+//
+//
+//
+//            startActivity(intent)
         }
     }
-}
